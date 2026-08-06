ab18baf7ba84e245c585455a9684f72d89515b4f
dqsfqf <guneseyoo@gmail.com>
2026-08-06 22:21:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // ── Config Discord ──────────────────────────────────────────────
 const DISCORD_CLIENT_ID     = "1534980941355683950";
 const DISCORD_CLIENT_SECRET = "sMPhtifc26xWQQMqZyw8DAE4qMF7HaQr"; // ← mets ton secret ici
-const DISCORD_REDIRECT_URI  = "http://127.0.0.1:3000/callback";
+const DISCORD_REDIRECT_URI  = "https://myapi-dxz9.onrender.com/callback";
 // ───────────────────────────────────────────────────────────────
 
 // Enregistre snow:// dans le registre Windows au démarrage
